6a5629302813034e758a80176de0561d14e7d90a
Gustavo_Gómez <gustavogomez08@gmail.com>
2026-05-25 16:27:32 -0300

    chore: remover scripts de prueba

--- test_type.py
+++
@@ -1,9 +0,0 @@
-import sys
-from zeep import Client
-
-def check_type():
-    c = Client('http://gps.rcontrol.com.mx/Tracking/wcf/RCService.svc?wsdl')
-    print(c.get_type('ns2:Event').signature())
-
-if __name__ == "__main__":
-    check_type()
