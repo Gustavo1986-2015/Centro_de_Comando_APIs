fe6a2d1b3a7d4c87ab61517c6ed89454d41f3d16
Gustavo_Gómez <gustavogomez08@gmail.com>
2026-06-25 16:44:49 -0300

    chore(logging): agregar colores a la consola con colorlog

--- app/core/logging_config.py
+++ app/core/logging_config.py
@@ -30,12 +30,24 @@ def setup_logging(env_path=".env"):
             "standard": {
                 "format": "%(asctime)s [%(levelname)s] [%(name)s] %(message)s",
                 "datefmt": "%Y-%m-%d %H:%M:%S"
+            },
+            "colored": {
+                "()": "colorlog.ColoredFormatter",
+                "format": "%(log_color)s%(asctime)s [%(levelname)s] [%(name)s] %(message)s",
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+                "log_colors": {
+                    "DEBUG": "cyan",
+                    "INFO": "green",
+                    "WARNING": "yellow",
+                    "ERROR": "red",
+                    "CRITICAL": "red,bg_white"
+                }
             }
         },
         "handlers": {
             "console": {
                 "class": "logging.StreamHandler",
-                "formatter": "standard",
+                "formatter": "colored",
                 "stream": "ext://sys.stdout"
             },
             "file": {
